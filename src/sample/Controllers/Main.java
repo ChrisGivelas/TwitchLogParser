8fb1cbe9eb71b8f8cd4cf4a407b7ee888a62e38d
Chris Givelas <chris_g_50@hotmail.com>
2018-12-07 11:18:08 -0500

    Minor changes

--- src/sample/Controllers/Main.java
+++ src/sample/Controllers/Main.java
@@ -4,7 +4,6 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import sample.Entities.AnalyzedLogFile;
